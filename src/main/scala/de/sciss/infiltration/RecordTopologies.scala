41610cb5a15993507a7db655168f550af987b1af
Hanns Holger Rutz <contact@sciss.de>
2020-06-08 18:44:42 +0200

    ...

---
+++ src/main/scala/de/sciss/infiltration/RecordTopologies.scala
@@ -0,0 +1,22 @@
+/*
+ *  RecordTopologies.scala
+ *  (Infiltration)
+ *
+ *  Copyright (c) 2019-2020 Hanns Holger Rutz. All rights reserved.
+ *
+ *  This software is published under the GNU Affero General Public License v3+
+ *
+ *
+ *  For further information, please contact Hanns Holger Rutz at
+ *  contact@sciss.de
+ */
+
+package de.sciss.infiltration
+
+import de.sciss.nuages.Wolkenpumpe
+
+object RecordTopologies {
+  def main(args: Array[String]): Unit = {
+    Wolkenpumpe.init()
+  }
+}
